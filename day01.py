90d24911e7520dbf2f61fccb1a086e630b47d2d6
unbe <unbe@users.noreply.github.com>
2025-12-02 01:03:21 +0100

    day 1 with many errors on the way, late after training

---
+++ day01.py
@@ -0,0 +1,21 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+  pos = 50
+  part1 = 0
+  part2 = 0
+  for line in f.readlines():
+    sign = 1 if line[0] == 'R' else -1
+    step = int(line[1:])
+    oldpos = pos
+    next = pos + sign * step
+    pos = next % 100
+    rounds = abs(next - pos) // 100
+    if (pos == 0 and sign > 0) or (oldpos == 0 and sign < 0):
+      rounds -= 1
+    part2 += rounds
+    if pos == 0:
+      part1 += 1
+      part2 += 1
+  print("part1:", part1)
+  print("part2:", part2)
